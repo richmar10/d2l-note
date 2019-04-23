b6b03cbb4eb309ae5b1919aef233cb86674254ba
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2019-04-23 14:28:12 -0400

    Add d2l-notes and d2l-note-edit

--- src/d2l-note-edit.ts
+++ src/d2l-note-edit.ts
@@ -1,4 +1,3 @@
-import './d2l-note-edit';
 /**
  * Import LitElement base class, html helper function,
  * and TypeScript decorators
@@ -11,14 +10,14 @@ import {
  * Use the customElement decorator to define your class as
  * a custom element. Registers <my-element> as an HTML tag.
  */
-@customElement('d2l-note')
-export class D2LNote extends LitElement {
+@customElement('d2l-note-edit')
+export class D2LNoteEdit extends LitElement {
 
 	/**
 	 * Create an observed property. Triggers update on change.
 	 */
 	@property()
-	prop1 = 'd2l-note';
+	prop1 = 'd2l-note-edit';
 
 	/**
 	 * Implement `render` to define a template for your element.
---
+++ src/d2l-notes.ts
@@ -0,0 +1,33 @@
+import './d2l-note';
+/**
+ * Import LitElement base class, html helper function,
+ * and TypeScript decorators
+ **/
+import {
+	customElement, html, LitElement, property
+} from 'lit-element';
+
+/**
+ * Use the customElement decorator to define your class as
+ * a custom element. Registers <my-element> as an HTML tag.
+ */
+@customElement('d2l-notes')
+export class D2LNotes extends LitElement {
+
+	/**
+	 * Create an observed property. Triggers update on change.
+	 */
+	@property()
+	prop1 = 'd2l-notes';
+
+	/**
+	 * Implement `render` to define a template for your element.
+	 */
+	render() {
+		/**
+		 * Use JavaScript expressions to include property values in
+		 * the element template.
+		 */
+		return html`<p>Hello ${this.prop1}!</p>`;
+	}
+}
