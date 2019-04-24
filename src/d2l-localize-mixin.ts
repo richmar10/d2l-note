6eaab8b0ee1a8d645f2de5f8c50abea60ed8b2f8
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2019-04-24 08:17:42 -0400

    Prelimary work on d2l-note component

---
+++ src/d2l-localize-mixin.ts
@@ -0,0 +1,64 @@
+import {
+	LitElement, property
+} from 'lit-element';
+
+import d2lIntl from 'd2l-intl';
+
+function tryParseHtmlElemAttr(attr: string | null, defaultValue: object) {
+	if (!attr) {
+		return defaultValue;
+	}
+	try {
+		return JSON.parse(attr) as object;
+	} catch (e) {
+		return defaultValue;
+	}
+}
+
+type Constructor<T> = { new (...args: any[]): T };
+export function localizeMixin<B extends Constructor<any>>(base: B) {
+	class LocalizeMixin extends base {
+		@property()
+		__documentLanguage = window.document.getElementsByTagName('html')[0]
+			.getAttribute('lang')
+
+		@property()
+		__documentLanguageFallback = window.document.getElementsByTagName('html')[0]
+			.getAttribute('data-lang-default');
+
+		@property()
+		__overrides = tryParseHtmlElemAttr(window.document.getElementsByTagName('html')[0].getAttribute('data-intl-overrides'), {});
+
+		@property()
+		__timezoneObject = tryParseHtmlElemAttr(window.document.getElementsByTagName('html')[0].getAttribute('data-timezone'), {name: '', identifier: ''});
+
+		_observer?: MutationObserver;
+
+		connectedCallback() {
+			const htmlElem = window.document.getElementsByTagName('html')[0];
+
+			this._observer = new MutationObserver(((mutations) => {
+				for (let i = 0; i < mutations.length; i++) {
+					const mutation = mutations[i];
+					if (mutation.attributeName === 'lang') {
+						this.__documentLanguage = htmlElem.getAttribute('lang');
+					} else if (mutation.attributeName === 'data-lang-default') {
+						this.__documentLanguageFallback = htmlElem.getAttribute('data-lang-default');
+					} else if (mutation.attributeName === 'data-intl-overrides') {
+						this.__overrides = tryParseHtmlElemAttr(htmlElem.getAttribute('data-intl-overrides'), {});
+					} else if (mutation.attributeName === 'data-timezone') {
+						this.__timezoneObject = tryParseHtmlElemAttr(htmlElem.getAttribute('data-timezone'), {name: '', identifier: ''});
+					}
+				}
+			}));
+			this._observer.observe(htmlElem, { attributes: true });
+		}
+
+		disconnectedCallback() {
+			if (this._observer && this._observer.disconnect) {
+				this._observer.disconnect();
+			}
+		}
+	}
+	return LocalizeMixin;
+}
--- src/d2l-note.ts
+++ src/d2l-note.ts
@@ -1,3 +1,4 @@
+import '@d2l/user-elements/d2l-user.js';
 import './d2l-note-edit';
 /**
  * Import LitElement base class, html helper function,
@@ -7,27 +8,74 @@ import {
 	customElement, html, LitElement, property
 } from 'lit-element';
 
+import { localizeMixin } from './d2l-localize-mixin';
+
 /**
  * Use the customElement decorator to define your class as
  * a custom element. Registers <my-element> as an HTML tag.
  */
 @customElement('d2l-note')
-export class D2LNote extends LitElement {
+export class D2LNote extends localizeMixin(LitElement) {
 
 	/**
 	 * Create an observed property. Triggers update on change.
 	 */
-	@property()
-	prop1 = 'd2l-note';
+	@property({ type: Object })
+	user?: {
+		pic?: {
+			url: string;
+			requireTokenAuth?: boolean;
+		},
+		name?: string;
+	};
+
+	@property({ type: String })
+	token?: string;
+
+	@property({ type: Boolean })
+	showavatar = true;
+
+	@property({ type: String })
+	createdat?: string;
+
+	@property({ type: String })
+	updatedat?: string;
+
+	@property({ type: String })
+	dateformat?: string;
+
+	@property({ type: String })
+	text?: string;
 
 	/**
 	 * Implement `render` to define a template for your element.
 	 */
 	render() {
+		function convertText(text: string) {
+			return html`<div class="d2l-note-text">${text}</div>`;
+		}
 		/**
 		 * Use JavaScript expressions to include property values in
 		 * the element template.
 		 */
-		return html`<p>Hello ${this.prop1}!</p>`;
+		const imageUrl = (this.showavatar && this.user && this.user.pic) ? this.user.pic.url : '';
+		const useImageAuthentication = !!(this.showavatar && this.user && this.user.pic && this.user.pic.requireTokenAuth);
+
+		const formatter = new d2lIntl.DateTimeFormat(this.language);
+		const date = formatter.format(this.createdat)
+			.format(this.dateformat || langterms('EvidenceDetails.DateFormat'));
+		const subText = this.updatedat ? langterms('TeacherComments.SubtextEdited', date) : date;
+		return html`
+			${this.user ? html`
+				<d2l-user
+			        image-url=${imageUrl}
+			        use-image-authentication=${useImageAuthentication}
+			        image-token=${useImageAuthentication ? this.token : ''}
+			        name=${this.user.name}
+			        sub-text=${subText}
+					></d2l-user>` : html`
+				<div class="d2l-note-user-skeleton"></div>`}
+			${this.text ? convertText(this.text) : html`<div class="d2l-note-text-skeleton"></div>`}
+		`;
 	}
 }
